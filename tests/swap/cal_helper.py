45e29dc2b77139ff2265d2b7a1929158ee583c28
Francois Beutin <francois.beutin@ledger.fr>
2025-07-24 16:36:11 +0200

    Add tests for LiFi swap

--- tests/swap/cal_helper.py
+++ tests/swap/cal_helper.py
@@ -10,4 +10,5 @@ from application_client import solana_utils as SOL
 SOL_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="SOL", conf=SOL.SOL_CONF, packed_derivation_path=SOL.SOL_PACKED_DERIVATION_PATH)
 JUP_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="JUP", conf=SOL.JUP_CONF, packed_derivation_path=SOL.JUP_PACKED_DERIVATION_PATH)
 SOL_USDC_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="USDC", conf=SOL.SOL_USDC_CONF, packed_derivation_path=SOL.SOL_USDC_PACKED_DERIVATION_PATH)
+GORK_CURRENCY_CONFIGURATION = CurrencyConfiguration(ticker="GORK", conf=SOL.GORK_CONF, packed_derivation_path=SOL.GORK_PACKED_DERIVATION_PATH)
 # --8<-- [end:sol_conf]
