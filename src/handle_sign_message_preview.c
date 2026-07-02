43e79e5d8e4a234a8f8bd433c037705f22a07b00
Francois Beutin <francois.beutin@ledger.fr>
2026-07-02 11:00:56 +0200

    wip

--- src/handle_sign_message_preview.c
+++ src/handle_sign_message_preview.c
@@ -14,6 +14,22 @@
 #include "io.h"
 #include "app_mem_utils.h"
 
+// Preview state for delayed signing
+typedef struct {
+    bool initialized;
+
+    // SHA-512 hash of the message with zeroed blockhash
+    uint8_t message_hash_with_zero_blockhash[CX_SHA512_SIZE];
+
+    // Not technically needed because we check that the hash matches but reduces probability of
+    // finding a collision
+    int message_length;
+
+    // Needed because not in the hashed data itself
+    uint32_t derivation_path_length;
+    uint32_t derivation_path[MAX_BIP32_PATH_LENGTH];
+} preview_state_t;
+
 preview_state_t *G_preview_state;
 
 void clear_preview_state(void) {
